f14709a1d54a0613dcdf5a90ad89dce8e606be65
paulovskeller <paulo.vinicius.keller@gmail.com>
2022-01-17 20:47:08 -0300

    Initial Commit

---
+++ SmashBrosUltimate/SmashBrosUltimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmashBrosUltimateApp.swift
+//  SmashBrosUltimate
+//
+//  Created by Paulo Keller on 17/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SmashBrosUltimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
